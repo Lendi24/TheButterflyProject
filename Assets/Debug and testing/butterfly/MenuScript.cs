943665094b8a41dfde7b4d288f0bd7215a9a2e04
Fojii <tiger.sebbe@hotmail.com>
2021-09-03 17:40:06 +0200

    Main Menu sends you to correct scene
    :)

--- Assets/Debug and testing/butterfly/MenuScript.cs
+++ Assets/Debug and testing/butterfly/MenuScript.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public void StartGame()
     {
-        EditorSceneManager.LoadScene("SpawnTest");
+        SceneManager.LoadScene("ButterHunt");
     }
 }
