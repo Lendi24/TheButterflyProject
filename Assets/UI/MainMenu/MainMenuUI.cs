2926668563c4a5930827118126271cce1e5a97c1
Lendi24 <Lendic24@hotmail.com>
2021-11-06 21:28:09 +0100

    Fixed dynamic scaling.. Maybe
    I think I fixed it. More testing and ironing bugs will have to be done. I calculated dpi stuff and changed expected screen-size according to that. I want to test a transition with a device of another dpi, tho. Just to see if it really works. And also, UI needs fixing. And I need to make it so it doesn't try changing the screen every frame.

--- Assets/UI/MainMenu/MainMenuUI.cs
+++ Assets/UI/MainMenu/MainMenuUI.cs
@@ -14,29 +14,17 @@ public class MainMenuUI : MonoBehaviour
     public StyleSheet ussSmallScreens;
     public StyleSheet ussBigScreens;
 
+    float expectedDpi;
+    bool changeRes;
+
     VisualElement root;
 
-    private void Update()
-    {
-        if (Screen.height < 420 || Screen.width < 470)
-        {
-            root.styleSheets.Add(ussSmallScreens);
-            root.styleSheets.Remove(ussBigScreens);
-            Debug.Log("Small screen");
-        }
-
-        else
-        {
-            root.styleSheets.Add(ussBigScreens);
-            root.styleSheets.Remove(ussSmallScreens);
-            Debug.Log("Big screen");
-
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
     {
+        changeRes = true;
+        expectedDpi = GetComponent<UIDocument>().panelSettings.referenceDpi;
         root = GetComponent<UIDocument>().rootVisualElement;
         startButton = root.Q<Button>("menu-button-start");
         loadButton = root.Q<Button>("menu-button-load");
@@ -44,6 +32,24 @@ public class MainMenuUI : MonoBehaviour
         creditsButton = root.Q<Button>("menu-button-credits");
 
         startButton.clicked += StartButtonPressed;
+    }
+
+    private void Update()
+    {
+
+        if (Screen.height / (Screen.dpi / expectedDpi) < 420 || Screen.width / (Screen.dpi / expectedDpi) < 470)
+        {
+            root.styleSheets.Add(ussSmallScreens);
+            root.styleSheets.Remove(ussBigScreens);
+            changeRes = false;
+        }
+
+        else
+        {
+            root.styleSheets.Add(ussBigScreens);
+            root.styleSheets.Remove(ussSmallScreens);
+            changeRes = false;
+        }
     }
 
     void StartButtonPressed()
