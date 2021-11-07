03820a7eec61f7888e324fbf0dfaa1b3c8beb008
Lendi24 <Lendic24@hotmail.com>
2021-11-07 15:22:22 +0100

    Fixed USS for start menu. Still needs work, tho

--- Assets/UI/MainMenu/MainMenuUI.cs
+++ Assets/UI/MainMenu/MainMenuUI.cs
@@ -10,21 +10,11 @@ public class MainMenuUI : MonoBehaviour
     public Button loadButton;
     public Button settingsButton;
     public Button creditsButton;
-
-    public StyleSheet ussSmallScreens;
-    public StyleSheet ussBigScreens;
-
-    float expectedDpi;
-    bool changeRes;
-
     VisualElement root;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        changeRes = true;
-        expectedDpi = GetComponent<UIDocument>().panelSettings.referenceDpi;
         root = GetComponent<UIDocument>().rootVisualElement;
         startButton = root.Q<Button>("menu-button-start");
         loadButton = root.Q<Button>("menu-button-load");
@@ -32,24 +22,6 @@ public class MainMenuUI : MonoBehaviour
         creditsButton = root.Q<Button>("menu-button-credits");
 
         startButton.clicked += StartButtonPressed;
-    }
-
-    private void Update()
-    {
-
-        if (Screen.height / (Screen.dpi / expectedDpi) < 420 || Screen.width / (Screen.dpi / expectedDpi) < 470)
-        {
-            root.styleSheets.Add(ussSmallScreens);
-            root.styleSheets.Remove(ussBigScreens);
-            changeRes = false;
-        }
-
-        else
-        {
-            root.styleSheets.Add(ussBigScreens);
-            root.styleSheets.Remove(ussSmallScreens);
-            changeRes = false;
-        }
     }
 
     void StartButtonPressed()
